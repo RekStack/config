529d40395908a121652ba39ac4146427b2864f51
Rui Araújo <ruiaraujo012@gmail.com>
2024-08-04 23:18:58 +0100

    fix(eslint): change no-unused-vars rule

--- packages/eslint-react/src/index.js
+++ packages/eslint-react/src/index.js
@@ -23,7 +23,9 @@ export default [
       'unused-imports': unusedImports,
     },
     rules: {
-      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      // 'no-unused-vars': 'off',
+      // or
+      '@typescript-eslint/no-unused-vars': 'off',
       'unused-imports/no-unused-imports': 'warn',
       'unused-imports/no-unused-vars': [
         'warn',

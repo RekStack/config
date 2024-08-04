a140bd3e025ae849bd718f83dbbdf0c091d2e32e
Rui Araújo <ruiaraujo012@gmail.com>
2024-08-04 19:02:37 +0100

    fix(eslint): fix missing config

--- packages/eslint-react/src/index.js
+++ packages/eslint-react/src/index.js
@@ -7,10 +7,11 @@ import pluginReact from 'eslint-plugin-react';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } }, settings: { react: { version: 'detect' } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
   // ...tanstackConfig,
   {
     // Custom rules go here

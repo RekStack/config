b32d6f5684bed484c91be7311339ee778acec358
Rui Araújo <ruiaraujo012@gmail.com>
2024-08-04 15:02:37 +0100

    feat(packages): add package.json

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,15 +1,13 @@
 import { branchConfigs, packages, rootDir } from './config.js';
 import { publish } from '@tanstack/config/publish';
 
-console.log('tag >> ', process.env.TAG);
-
 await publish({
   branch: process.env.BRANCH,
   branchConfigs,
   ghToken: process.env.GH_TOKEN,
   packages,
   rootDir,
-  // tag: process.env.TAG,
+  tag: process.env.TAG,
 })
   .then(() => {
     console.log('Successfully published packages!');

c38859d7a4d20aaab10ad999d8ee37a9babe5ffc
Rui Araújo <ruiaraujo012@gmail.com>
2024-08-04 19:01:50 +0100

    feat(eslint-react): add eslint v9 with some configs

--- packages/eslint-react/src/index.js
+++ packages/eslint-react/src/index.js
@@ -1,8 +1,17 @@
-import { tanstackConfig } from '@tanstack/config/eslint';
+// import { tanstackConfig } from '@tanstack/config/eslint';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  ...tanstackConfig,
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  // ...tanstackConfig,
   {
     // Custom rules go here
   },

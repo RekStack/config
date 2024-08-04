e304272285f9ce35b3a167299ab7871e89b5a667
Rui Araújo <ruiaraujo012@gmail.com>
2024-08-04 23:51:54 +0100

    fix(eslint): dep

--- packages/eslint-react/src/index.js
+++ packages/eslint-react/src/index.js
@@ -5,6 +5,7 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import unusedImports from 'eslint-plugin-unused-imports';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+
 // import { tanstackConfig } from '@tanstack/config/eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
